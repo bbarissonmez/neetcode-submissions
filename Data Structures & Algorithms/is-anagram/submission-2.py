bd7c59b33d7125a36ba97d9c14e22c9fc502ba52
Baris Sonmez <zcabbso@ucl.ac.uk>
2026-07-26 03:41:55 +0300

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_set = {}
+        t_set = {}
+
+        for char in s:
+            if char not in s_set:
+                s_set[char] = 1
+            else:
+                s_set[char] += 1
+
+        for char in t:
+            if char not in t_set:
+                t_set[char] = 1
+            else:
+                t_set[char] += 1
+                
+        return s_set == t_set
+
+        
