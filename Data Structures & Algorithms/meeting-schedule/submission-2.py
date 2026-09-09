27201b5d8f477a3347ccdfe11307e854237fa518
Baris Sonmez <zcabbso@ucl.ac.uk>
2026-09-09 14:36:10 +0300

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.py
@@ -0,0 +1,22 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if len(intervals) == 0 or len(intervals) == 1:
+            return True
+        
+        intervals.sort(key = lambda x: x.start)
+        
+        for i in range (len(intervals) - 1):
+            if (intervals[i+1].end < intervals[i].end) or (intervals[i+1].start < intervals[i].end):
+                return False
+
+        return True
+
+        
